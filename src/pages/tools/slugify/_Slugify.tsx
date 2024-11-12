3de6eb21e3d8db85c0ef999727d7f3b67b8299dc
Shubh A Chudasama <shubh.cs@proton.me>
2024-11-12 22:13:33 +0530

    Avoid SSR on slugify tool

--- src/pages/tools/slugify/_Slugify.tsx
+++ src/pages/tools/slugify/_Slugify.tsx
@@ -1,3 +1,4 @@
+import BrowserOnly from "@docusaurus/BrowserOnly";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import Box from "@mui/material/Box";
 import IconButton from "@mui/material/IconButton";
@@ -30,35 +31,39 @@ export default function Slugify() {
   };
 
   return (
-    <Box sx={{ width: "100%", mt: 2 }}>
-      <TextField
-        label="Enter text here"
-        variant="outlined"
-        multiline
-        minRows={1}
-        fullWidth
-        onChange={handleChange}
-      />
-      <Box sx={{ mt: 2 }}>
-        <Stack direction={"row"} spacing={1} alignItems={"center"}>
-          <h3>Output</h3>
-          <IconButton
-            size="medium"
-            onClick={async () => {
-              if (await copyToClipboard(output.join("\n"))) {
-                successToast("Copied text.");
-              }
-            }}
-          >
-            <ContentCopyIcon fontSize="inherit" />
-          </IconButton>
-        </Stack>
-        <ul>
-          {output.map((line, index) => (
-            <li key={index}>{line}</li>
-          ))}
-        </ul>
-      </Box>
-    </Box>
+    <BrowserOnly>
+      {() => (
+        <Box sx={{ width: "100%", mt: 2 }}>
+          <TextField
+            label="Enter text here"
+            variant="outlined"
+            multiline
+            minRows={1}
+            fullWidth
+            onChange={handleChange}
+          />
+          <Box sx={{ mt: 2 }}>
+            <Stack direction={"row"} spacing={1} alignItems={"center"}>
+              <h3>Output</h3>
+              <IconButton
+                size="medium"
+                onClick={async () => {
+                  if (await copyToClipboard(output.join("\n"))) {
+                    successToast("Copied text.");
+                  }
+                }}
+              >
+                <ContentCopyIcon fontSize="inherit" />
+              </IconButton>
+            </Stack>
+            <ul>
+              {output.map((line, index) => (
+                <li key={index}>{line}</li>
+              ))}
+            </ul>
+          </Box>
+        </Box>
+      )}
+    </BrowserOnly>
   );
 }
