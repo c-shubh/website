fad4aff0c30a729b907176a325608fbf4861659c
Shubh A Chudasama <shubh.cs@proton.me>
2025-06-25 21:23:41 +0530

    qrcode generator: support bulk qr generation

--- src/pages/tools/qr-code-generator/_PlainTextQrCodeGenerator.tsx
+++ src/pages/tools/qr-code-generator/_PlainTextQrCodeGenerator.tsx
@@ -2,6 +2,8 @@ import BrowserOnly from "@docusaurus/BrowserOnly";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import DownloadIcon from "@mui/icons-material/Download";
+import FormatListNumberedIcon from "@mui/icons-material/FormatListNumbered";
+import LooksOneOutlinedIcon from "@mui/icons-material/LooksOneOutlined";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import ButtonGroup from "@mui/material/ButtonGroup";
@@ -14,6 +16,8 @@ import Paper from "@mui/material/Paper";
 import Popper from "@mui/material/Popper";
 import Stack from "@mui/material/Stack";
 import TextField from "@mui/material/TextField";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 import { successToast } from "@site/src/utils";
 import { QRCode } from "antd";
 import * as React from "react";
@@ -26,10 +30,18 @@ import {
 
 export default function PlainTextQrCodeGenerator() {
   const [text, setText] = useState("");
+  const [count, setCount] = React.useState<string | null>("one");
   const options = ["PNG", "SVG"];
   const PNG = 0,
     SVG = 1;
   const [selectedIndex, setSelectedIndex] = React.useState(PNG);
+
+  const handleCountChange = (
+    event: React.MouseEvent<HTMLElement>,
+    newAlignment: string | null
+  ) => {
+    setCount(newAlignment);
+  };
 
   // TODO: wifi and upi qr code
   return (
@@ -41,60 +53,103 @@ export default function PlainTextQrCodeGenerator() {
             size="small"
             placeholder="Enter text"
             variant="outlined"
-            maxRows={5}
+            maxRows={10}
             value={text}
             onChange={(e) => setText(e.target.value)}
             multiline
             autoFocus
           />
-          <Grid
-            container
-            sx={{
-              gap: 2,
-              justifyContent: { xs: "center", sm: "start" },
-            }}
+          <ToggleButtonGroup
+            value={count}
+            exclusive
+            onChange={handleCountChange}
+            size="small"
           >
-            <Grid item>
-              <Box
-                id="myqrcode"
-                component={QRCode}
-                value={text || "hello"}
-                bgColor="#fff"
-                size={200}
-                type={selectedIndex === SVG ? "svg" : "canvas"}
-              ></Box>
+            <ToggleButton value="one" sx={{ fontWeight: "bold" }}>
+              <LooksOneOutlinedIcon />
+              &nbsp; Single
+            </ToggleButton>
+            <ToggleButton value="many" sx={{ fontWeight: "bold" }}>
+              <FormatListNumberedIcon />
+              &nbsp; Multiple
+            </ToggleButton>
+          </ToggleButtonGroup>
+          {count === "one" && (
+            <Grid
+              container
+              sx={{
+                gap: 2,
+                justifyContent: { xs: "center", sm: "start" },
+              }}
+            >
+              <Grid item>
+                <Box
+                  id="myqrcode"
+                  component={QRCode}
+                  value={text || "hello"}
+                  bgColor="#fff"
+                  size={300}
+                  type={selectedIndex === SVG ? "svg" : "canvas"}
+                ></Box>
+              </Grid>
+              <Grid item>
+                <Stack spacing={2}>
+                  <Button
+                    variant="outlined"
+                    startIcon={<ContentCopyIcon />}
+                    size="small"
+                    onClick={async () => {
+                      if (selectedIndex === SVG) {
+                        setSelectedIndex(PNG);
+                      }
+                      await copyCanvasQRCode();
+                      successToast("Copied to clipboard");
+                    }}
+                  >
+                    Copy to Clipboard
+                  </Button>
+                  <SplitButton
+                    options={options}
+                    selectedIndex={selectedIndex}
+                    setSelectedIndex={setSelectedIndex}
+                    onClick={() => {
+                      if (selectedIndex === SVG) {
+                        downloadSvgQRCode();
+                      } else if (selectedIndex === PNG) {
+                        downloadCanvasQRCode();
+                      }
+                    }}
+                  />
+                </Stack>
+              </Grid>
             </Grid>
-            <Grid item>
-              <Stack spacing={2}>
-                <Button
-                  variant="outlined"
-                  startIcon={<ContentCopyIcon />}
-                  size="small"
-                  onClick={async () => {
-                    if (selectedIndex === SVG) {
-                      setSelectedIndex(PNG);
-                    }
-                    await copyCanvasQRCode();
-                    successToast("Copied to clipboard");
-                  }}
-                >
-                  Copy to Clipboard
-                </Button>
-                <SplitButton
-                  options={options}
-                  selectedIndex={selectedIndex}
-                  setSelectedIndex={setSelectedIndex}
-                  onClick={() => {
-                    if (selectedIndex === SVG) {
-                      downloadSvgQRCode();
-                    } else if (selectedIndex === PNG) {
-                      downloadCanvasQRCode();
-                    }
-                  }}
-                />
-              </Stack>
-            </Grid>
-          </Grid>
+          )}
+          {count === "many" && (
+            <Stack alignItems="center" spacing={2} width={"100%"}>
+              {(text.trim() === "" ? ["hello"] : text.trim().split("\n")).map(
+                (line, index) => (
+                  <React.Fragment key={index}>
+                    <Box>
+                      QR Code #{index + 1}.{" "}
+                      <Box
+                        component={"code"}
+                        sx={{ display: "inline-block", width: "fit-content" }}
+                      >
+                        {line}
+                      </Box>
+                    </Box>
+                    <Box
+                      component={QRCode}
+                      value={line || "hello"}
+                      bgColor="#fff"
+                      size={300}
+                      type={"canvas"}
+                    />
+                  </React.Fragment>
+                )
+              )}
+            </Stack>
+          )}
         </Stack>
       )}
     </BrowserOnly>
