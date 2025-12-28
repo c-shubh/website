4623e3abb2dcbe2d1c28ddecf892adbe74e9af4f
Shubh A Chudasama <shubh.cs@proton.me>
2025-12-28 21:53:52 +0530

    Impl navbar, add tailwind, normalize

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,14 @@
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://cshubh.com',
 	integrations: [mdx(), react(), sitemap()],
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
