e84bc6f576ce48d3ab1b77af2807a7f188f847a6
Shubh A Chudasama <shubh.cs@proton.me>
2026-01-24 21:07:23 +0530

    Add archived library page

---
+++ src/pages/library/_data.ts
@@ -0,0 +1,169 @@
+interface Writing {
+	date: string;
+	title: string;
+	url: string;
+	hnUrl?: string;
+	archiveUrl?: string;
+}
+
+export const writings: Writing[] = [
+	{
+		date: '2024-09-15',
+		title: 'Reducing writing friction',
+		url: 'https://qmacro.org/blog/posts/2020/09/11/reducing-writing-friction/',
+	},
+	{
+		date: '2024-09-15',
+		title: '15 rules for blogging, and my current streak',
+		url: 'https://interconnected.org/home/2020/09/10/streak',
+	},
+	{
+		date: '2024-09-15',
+		title: 'What Causes Mental Illness?',
+		url: 'https://isha.sadhguru.org/en/wisdom/sadhguru-spot/understanding-mental-illness',
+	},
+	{
+		date: '2024-09-12',
+		title: 'The Surprising Value of Rest',
+		url: 'https://zenhabits.net/restful/',
+	},
+	{
+		date: '2024-09-12',
+		title: 'The Moment That Ruins Our Focus',
+		url: 'https://zenhabits.net/moment-to-focus/',
+	},
+	{ date: '2024-09-12', title: 'The Joys of Getting Organized', url: 'https://zenhabits.net/joy/' },
+	{
+		date: '2024-09-12',
+		title: 'Simply Getting Things Done',
+		url: 'https://donebeforebrekky.com/simply-getting-things-done/',
+		archiveUrl:
+			'http://web.archive.org/web/20190328185307/https://donebeforebrekky.com/simply-getting-things-done/',
+	},
+	{ date: '2024-09-12', title: 'Ritual to Greet the Day', url: 'https://zenhabits.net/greet/' },
+	{
+		date: '2024-09-12',
+		title: 'My Morning Routine',
+		url: 'https://zenhabits.net/my-morning-routine/',
+	},
+	{
+		date: '2024-09-12',
+		title: 'How to Develop a Strong Sense of Self',
+		url: 'https://zenhabits.net/strong-sense-of-self/',
+	},
+	{
+		date: '2024-09-12',
+		title: 'How to Break Dependence on the Phone',
+		url: 'https://zenhabits.net/break-phone-dependence/',
+	},
+	{
+		date: '2024-09-12',
+		title: 'How to Address What’s Really Causing Your Avoidance',
+		url: 'https://zenhabits.net/whats-causing-your-avoidance/',
+	},
+	{
+		date: '2024-09-12',
+		title: 'GTD in 15 minutes – A Pragmatic Guide to Getting Things Done',
+		url: 'https://hamberg.no/gtd/',
+	},
+	{
+		date: '2024-09-12',
+		title: 'Get Good at Finishing Things',
+		url: 'https://zenhabits.net/finishing/',
+	},
+	{
+		date: '2024-09-11',
+		title: 'Why Not Comments',
+		url: 'https://buttondown.com/hillelwayne/archive/why-not-comments/',
+		hnUrl: 'https://news.ycombinator.com/item?id=41505389',
+	},
+	{
+		date: '2024-09-11',
+		title: 'Lunch with Alan Kay: how to become educated enough to invent the future',
+		url: 'https://futureofcoding.org/notes/alan-kay-lunch',
+	},
+	{
+		date: '2024-09-11',
+		title: 'How GitHub replaced SourceForge as the dominant code hosting platform',
+		url: 'https://graphite.dev/blog/github-monopoly-on-code-hosting',
+	},
+	{
+		date: '2024-09-11',
+		title: 'Comment the Why *and* the What',
+		url: 'https://buttondown.com/hillelwayne/archive/3866bd6e-22c3-4098-92ef-4d47ef287ed8',
+	},
+	{
+		date: '2024-09-11',
+		title: 'Building the Same App Using Various Web Frameworks',
+		url: 'https://eugeneyan.com/writing/web-frameworks/',
+
+		hnUrl: 'https://news.ycombinator.com/item?id=41507271',
+	},
+	{
+		date: '2024-09-10',
+		title: 'Windows NT vs. Unix: A design comparison',
+		url: 'https://blogsystem5.substack.com/p/windows-nt-vs-unix-design',
+		hnUrl: 'https://news.ycombinator.com/item?id=41490290',
+	},
+	{
+		date: '2024-09-10',
+		title: 'The Matasano Crypto Challenges',
+		url: 'https://blog.pinboard.in/2013/04/the_matasano_crypto_challenges/',
+	},
+	{ date: '2024-09-10', title: 'Mind Like Water', url: 'https://zenhabits.net/mind-like-water/' },
+	{
+		date: '2024-09-10',
+		title: 'Just for Fun. No, Really.',
+		url: 'https://justforfunnoreally.dev/',
+		hnUrl: 'https://news.ycombinator.com/item?id=41496725',
+	},
+	{
+		date: '2024-09-10',
+		title: 'How to Prepare for a Capture the Flag Hacking Competition',
+		url: 'https://www.cbtnuggets.com/blog/training/exam-prep/how-to-prepare-for-a-capture-the-flag-hacking-competition',
+	},
+	{
+		date: '2024-09-10',
+		title: 'How I Built an NFC Movie Library for my Kids',
+		url: 'https://simplyexplained.com/blog/how-i-built-an-nfc-movie-library-for-my-kids/',
+		hnUrl: 'https://news.ycombinator.com/item?id=41479141',
+	},
+	{
+		date: '2024-09-10',
+		title: 'Go 1.16 will make system calls through libc on OpenBSD',
+		url: 'https://utcc.utoronto.ca/~cks/space/blog/programming/Go116OpenBSDUsesLibc',
+	},
+	{
+		date: '2024-09-10',
+		title: 'Drawbridge - Microsoft Research',
+		url: 'https://www.microsoft.com/en-us/research/project/drawbridge/',
+	},
+	{ date: '2024-09-09', title: 'Breathe by Leo Babauta', url: 'https://zenhabits.net/breathe/' },
+	{
+		date: '2024-09-08',
+		title: 'OAuth from First Principles',
+		url: 'https://stack-auth.com/blog/oauth-from-first-principles',
+		hnUrl: 'https://news.ycombinator.com/item?id=41420783',
+	},
+	{
+		date: '2024-09-03',
+		title: 'Optimizing Javascript for fun and for profit',
+		url: 'https://romgrk.com/posts/optimizing-javascript',
+	},
+	{
+		date: '2024-09-02',
+		title: 'Brute force to DP solution of Longest Common Subsequence',
+		url: 'https://ics.uci.edu/~eppstein/161/960229.html',
+	},
+	{
+		date: '2024-08-25',
+		title: 'TIL: 8 versions of UUID and when to use them',
+		url: 'https://ntietz.com/blog/til-uses-for-the-different-uuid-versions/',
+		hnUrl: 'https://news.ycombinator.com/item?id=41350225',
+	},
+	{
+		date: '2024-08-23',
+		title: 'Why and how to write things on the Internet',
+		url: 'https://www.benkuhn.net/writing/',
+	},
+];
