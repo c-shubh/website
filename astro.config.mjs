daa000128e9724455cecaea33609d7a5d4d30bc8
Shubh A Chudasama <shubh.cs@proton.me>
2026-01-24 20:27:43 +0530

    Redirect from /gram to my instagram profile

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,5 +20,12 @@ export default defineConfig({
 		// This website used to have a /links page with list of links to interesting stuff.
 		// Now it's merged into /library.
 		'/links': '/library',
+		// /gram used to have a three column masonry layout of the same photos as
+		// Instagram. This was intended to be an alternative way to view the photos
+		// for those not on Instagram or who didn't want to use Instagram.
+		// This page was archived and now removed since converted HEIC photos were
+		// too large to load, and figuring out an optimal way is not a priority
+		// right now. I'll revisit this sometime in the future.
+		'/gram': 'https://www.instagram.com/c_shubh_/',
 	},
 });
