115c4a74e03a363c6b5aa3f1bb89cb2b40e1794a
Shubh A Chudasama <shubh.cs@proton.me>
2026-01-25 23:10:24 +0530

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,39 @@
 // @ts-check
 
-import { SITE } from './src/consts';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
+import { copyFile } from 'node:fs/promises';
+import { SITE } from './src/consts';
 
 // https://astro.build/config
 export default defineConfig({
 	site: SITE,
-	integrations: [mdx(), react(), sitemap()],
+	integrations: [
+		mdx(),
+		react(),
+		sitemap({
+			filter: (page) => page !== `${SITE}/library`,
+		}),
+		{
+			// Docusaurus generated sitemap at /sitemap.xml, but Astro generates it at
+			// /sitemap-index.xml. So copy it over after build to not break existing
+			// sitemap link.
+			name: 'copy-sitemap',
+			hooks: {
+				'astro:build:done': async ({ dir, logger }) => {
+					const inFile = new URL('sitemap-index.xml', dir);
+					const outFile = new URL('sitemap.xml', dir);
+					await copyFile(inFile, outFile);
+					logger.info('Copied `dist/sitemap-index.xml` to `dist/sitemap.xml`');
+				},
+			},
+		},
+		,
+	],
+	trailingSlash: 'never',
 	vite: {
 		plugins: [tailwindcss()],
 	},
@@ -28,5 +51,10 @@ export default defineConfig({
 		// too large to load, and figuring out an optimal way is not a priority
 		// right now. I'll revisit this sometime in the future.
 		'/gram': 'https://www.instagram.com/c_shubh_/',
+		// authors, archive and tags pages are yet to be implemented
+		'/blog/authors': '/blog',
+		'/blog/archive': '/blog',
+		'/blog/tags': '/blog',
+		'/blog/tags/til': '/blog',
 	},
 });
