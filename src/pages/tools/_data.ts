a1ef9e150f8c7487c59e1f9c8987399925460aa7
Shubh A Chudasama <shubh.cs@proton.me>
2026-02-21 22:57:47 +0530

    Tools: sort by name

--- src/pages/tools/_data.ts
+++ src/pages/tools/_data.ts
@@ -1,34 +1,11 @@
+// prettier-ignore
 export const tools = [
-	{
-		name: 'Bulk URL Opener',
-		slug: 'bulk-url-opener',
-		description: 'Paste a list of URLs to open them all in new tabs.',
-	},
-	{
-		name: 'QR Code Generator',
-		slug: 'qr-code-generator',
-		description: 'Generate one or more QR codes.',
-	},
-	{
-		name: 'Slugify',
-		slug: 'slugify',
-		description: 'Create slug from text.',
-	},
-	{
-		name: 'Word Count',
-		slug: 'word-count',
-		description: 'Count characters, words, and lines in text.',
-	},
-	{
-		name: 'Section Comment Generator',
-		slug: 'section-comment-generator',
-		description: 'Generate code comment to be used to mark sections in code.',
-	},
-	{
-		name: 'Camera & Mic Test',
-		slug: 'webcam-mic-test',
-		description: 'Test your camera and microphone, take pictures, or record audio.',
-	},
+	{ name: 'Bulk URL Opener', slug: 'bulk-url-opener', description: 'Paste a list of URLs to open them all in new tabs.' },
+	{ name: 'Camera & Mic Test', slug: 'webcam-mic-test', description: 'Test your camera and microphone, take pictures, or record audio.' },
+	{ name: 'QR Code Generator', slug: 'qr-code-generator', description: 'Generate one or more QR codes.' },
+	{ name: 'Section Comment Generator', slug: 'section-comment-generator', description: 'Generate code comment to be used to mark sections in code.' },
+	{ name: 'Slugify', slug: 'slugify', description: 'Create slug from text.' },
+	{ name: 'Word Count', slug: 'word-count', description: 'Count characters, words, and lines in text.' },
 ] as const;
 
 export type ToolSlug = (typeof tools)[number]['slug'];
