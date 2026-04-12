f8d0064d39110e8e7b65553547dd712443f511dd
Shubh A Chudasama <shubh.cs@proton.me>
2026-04-12 19:37:14 +0530

    Style section comment generator tool with daisyui

--- src/pages/tools/section-comment-generator/_SectionCommentGenerator.tsx
+++ src/pages/tools/section-comment-generator/_SectionCommentGenerator.tsx
@@ -1,5 +1,5 @@
 import { CopyButton } from '@/components/CopyButton';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, type ChangeEvent } from 'react';
 
 const styles = {
 	C: {
@@ -9,6 +9,10 @@ const styles = {
 	Python: {
 		start: '#',
 		end: '#',
+	},
+	SQL: {
+		start: '--',
+		end: '--',
 	},
 	Custom: {
 		start: '',
@@ -27,10 +31,10 @@ function generate(
 	text: string,
 	style: CommentStyleWithName,
 	lineLength: number,
-	indentationSize: number,
+	indentationSize: number
 ) {
 	if (!text) {
-		text = 'Hey there!';
+		return '';
 	}
 
 	const open = style.style.start;
@@ -80,25 +84,33 @@ export function SectionCommentGenerator() {
 	useEffect(refreshOutput, [text, lineLength, indentationSize, commentStyle]);
 
 	return (
-		<div>
-			<input
-				type="text"
-				placeholder="Enter text"
-				value={text}
-				onChange={(e) => setText(e.target.value)}
-				autoFocus
-			/>
-
+		<div className="flex flex-col gap-6">
 			<div>
-				<span>Style</span>
-				<div>
+				<label htmlFor="comment-text" className="sr-only">
+					Comment Text
+				</label>
+				<input
+					id="comment-text"
+					type="text"
+					className="input w-full"
+					placeholder="Enter text"
+					value={text}
+					onChange={(e) => setText(e.target.value)}
+					autoFocus
+				/>
+			</div>
+
+			<fieldset className="flex flex-col gap-4">
+				<legend className="font-medium">Style</legend>
+				<div className="flex flex-wrap gap-4 items-center h-full">
 					{Object.keys(styles).map((style) => (
-						<label key={style}>
+						<label key={style} className="inline-flex items-center gap-2">
 							<input
 								type="radio"
 								name="comment-style"
 								value={style}
 								checked={commentStyle.name === style}
+								className="radio"
 								// @ts-ignore
 								onChange={() => setCommentStyle({ name: style, style: styles[style] })}
 							/>
@@ -106,75 +118,91 @@ export function SectionCommentGenerator() {
 						</label>
 					))}
 				</div>
-			</div>
+			</fieldset>
 
-			<div>
-				<div>
-					<label htmlFor="start-token">Start</label>
-					<input
-						type="text"
-						id="start-token"
-						value={commentStyle.style.start}
-						disabled={commentStyle.name !== 'Custom'}
-						onChange={(e) =>
+			{[
+				[
+					{
+						id: 'start-token',
+						type: 'text',
+						label: 'Start',
+						value: commentStyle.style.start,
+						disabled: commentStyle.name !== 'Custom',
+						fn: (e: ChangeEvent<HTMLInputElement, HTMLInputElement>) =>
 							setCommentStyle((prev) => ({
 								...prev,
 								style: { ...prev.style, start: e.target.value },
-							}))
-						}
-					/>
-				</div>
-				<div>
-					<label htmlFor="end-token">End</label>
-					<input
-						type="text"
-						id="end-token"
-						value={commentStyle.style.end}
-						disabled={commentStyle.name !== 'Custom'}
-						onChange={(e) =>
+							})),
+					},
+					{
+						id: 'end-token',
+						type: 'text',
+						label: 'End',
+						disabled: commentStyle.name !== 'Custom',
+						value: commentStyle.style.end,
+						fn: (e: ChangeEvent<HTMLInputElement, HTMLInputElement>) =>
 							setCommentStyle((prev) => ({
 								...prev,
 								style: { ...prev.style, end: e.target.value },
-							}))
-						}
-					/>
-				</div>
-			</div>
-
-			<div>
-				<div>
-					<label htmlFor="line-length">Line Length</label>
-					<input
-						type="number"
-						id="line-length"
-						value={lineLength}
-						onChange={(e) => {
+							})),
+					},
+				],
+				[
+					{
+						id: 'line-length',
+						type: 'number',
+						label: 'Line Length',
+						disabled: false,
+						value: lineLength,
+						fn: (e: ChangeEvent<HTMLInputElement, HTMLInputElement>) => {
 							// @ts-expect-error: number input
 							const value = e.target.value as number;
 							setLineLength(Math.max(0, value).toString());
-						}}
-					/>
-				</div>
-
-				<div>
-					<label htmlFor="indent-size">Indentation</label>
-					<input
-						type="number"
-						id="indent-size"
-						value={indentationSize}
-						onChange={(e) => {
+						},
+					},
+					{
+						id: 'indent-size',
+						type: 'number',
+						label: 'Indentation',
+						disabled: false,
+						value: indentationSize,
+						fn: (e: ChangeEvent<HTMLInputElement, HTMLInputElement>) => {
 							// @ts-expect-error: number input
 							const value = e.target.value as number;
 							setIndentationSize(Math.max(0, value).toString());
-						}}
-					/>
+						},
+					},
+				],
+			].map((row, idx) => (
+				<div className="flex gap-4" key={idx}>
+					{row.map(({ id, label, type, value, disabled, fn }, idx) => (
+						<div className="flex-1 flex flex-col gap-1" key={idx}>
+							<label htmlFor={id} className="font-medium">
+								{label}
+							</label>
+							<input
+								type={type}
+								id={id}
+								className="w-full input"
+								value={value}
+								disabled={disabled}
+								onChange={fn}
+							/>
+						</div>
+					))}
 				</div>
-			</div>
-			<div>
-				<h3>Output</h3>
+			))}
+
+			<div className="flex items-center gap-2">
+				<h2 className="mt-0 mb-0">Output</h2>
 				<CopyButton getText={() => output} />
 			</div>
-			{output && <pre>{output}</pre>}
+
+			{output && (
+				<output htmlFor="comment-text line-length indent-size start-token end-token">
+					<pre className="w-full mt-0 mb-0 overflow-x-auto select-all">{output}</pre>
+				</output>
+			)}
 		</div>
 	);
 }
