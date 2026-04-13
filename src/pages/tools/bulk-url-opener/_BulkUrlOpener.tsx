d890f9421c0c9ba425d949c2939494d9d542e402
Shubh A Chudasama <shubh.cs@proton.me>
2026-04-13 23:27:48 +0530

    Extract icons into standalone components

--- src/pages/tools/bulk-url-opener/_BulkUrlOpener.tsx
+++ src/pages/tools/bulk-url-opener/_BulkUrlOpener.tsx
@@ -1,3 +1,4 @@
+import { XCircle } from '@/components/icons/XCircle';
 import { useState } from 'react';
 
 export function BulkUrlOpener() {
@@ -67,19 +68,7 @@ export function BulkUrlOpener() {
 			<div className="space-y-4">
 				{errors.map((error, idx) => (
 					<div role="alert" className="alert alert-error" key={idx}>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							className="h-6 w-6 shrink-0 stroke-current"
-							fill="none"
-							viewBox="0 0 24 24"
-						>
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								strokeWidth="2"
-								d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-							/>
-						</svg>
+						<XCircle />
 						<span>{error}</span>
 					</div>
 				))}
